c0f96b815085879e3586aef91dc65223b37955bb
Eric Nova <ericnova23@hotmail.com>
2023-11-15 07:50:09 -0600

    Section 10 11/17

--- src/app/dashboard/rest-todos/page.tsx
+++ src/app/dashboard/rest-todos/page.tsx
@@ -1,8 +1,18 @@
+import prisma from "@/lib/prisma";
+import { TodosGrid } from "@/todos";
+import { Metadata } from "next";
 
-export default function RestTodosPage() {
+export const metadata: Metadata = {
+  title: 'Todos List',
+  description: 'List of the TODOS'
+}
+
+export default async function RestTodosPage() {
+
+  const todos = await prisma.todo.findMany({ orderBy: { description: 'asc' } })
   return (
     <div>
-      <h1>RestTodos Page</h1>
+      <TodosGrid todos={todos} />
     </div>
   );
 }
