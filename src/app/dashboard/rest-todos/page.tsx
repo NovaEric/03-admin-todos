54aac9b7a27db57c6bcc48a7cfbb819c3d6f9caf
Eric Nova <ericnova23@hotmail.com>
2023-11-16 07:40:15 -0600

    Section 10 14/17

--- src/app/dashboard/rest-todos/page.tsx
+++ src/app/dashboard/rest-todos/page.tsx
@@ -1,5 +1,5 @@
 import prisma from "@/lib/prisma";
-import { TodosGrid } from "@/todos";
+import { NewTodo, TodosGrid } from "@/todos";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -12,6 +12,9 @@ export default async function RestTodosPage() {
   const todos = await prisma.todo.findMany({ orderBy: { description: 'asc' } })
   return (
     <div>
+      <div className="w-full px-3 mx-5 mb-5">
+        <NewTodo/>
+      </div>
       <TodosGrid todos={todos} />
     </div>
   );
---
+++ src/todos/components/NewTodo.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import { FormEvent, useState } from "react";
+import { IoTrashOutline } from "react-icons/io5";
+
+
+export const NewTodo = () => { 
+  
+  const [description, setDescription] = useState('');
+
+  const onSubmit = async(e: FormEvent) => {
+    e.preventDefault();
+    if (description.trim().length === 0) return;
+  }
+
+  return (
+    <form onSubmit={onSubmit} className='flex w-full'>
+      <input type="text"
+        onChange={ (e) => setDescription(e.target.value)}
+        className="w-6/12 -ml-10 pl-3 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-sky-500 transition-all"
+        placeholder="Add Task" />
+
+      <button type='submit' className="flex items-center justify-center rounded ml-2 bg-sky-500 p-2 text-white hover:bg-sky-700 transition-all">
+        Add
+      </button>
+      
+      <span className='flex flex-1'></span>
+
+      <button 
+        //TODO: onClick={ () => deleteCompleted() }
+        type='button' className="flex items-center justify-center rounded ml-2 bg-red-400 p-2 text-white hover:bg-red-700 transition-all">
+        <IoTrashOutline />
+        Delete
+      </button>
+
+
+    </form>
+  )
+}
